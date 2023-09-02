c54878aaf837ad98436010cb879bb9c1939580bb
Joao-Matias <jdmatias@live.com>
2023-09-02 18:30:14 +0100

    .

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const score1EL = document.getElementById(`score--1`);
 const current0EL = document.getElementById(`current--0`);
 const current1EL = document.getElementById(`current--1`);
 const diceEL = document.querySelector(`.dice`);
-
+qw;
 const btnNew = document.querySelector(`.btn--new`);
 const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
